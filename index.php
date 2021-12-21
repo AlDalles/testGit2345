94ee93138a8a3b385b63db4e44dfdf4b82b9b006
Leit040 <leit2040@gmail.com>
2021-12-21 21:04:42 +0200

    init

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$a = 10;
+$b = 20;
+$c = $a+$b;
+
+echo $a.PHP_EOL;
+echo $b.PHP_EOL;
+echo $c.PHP_EOL;
+$string ="test string";
+
+//int $number = 25;
+//float $num2 = 2.53;
+//double $num
+//boolean true /false
+//string ;
+
+
